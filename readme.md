8c5a9a1d4e665b7fdc063b54d242c80968b48deb
Zsolt Meszaros <zsolt@sowad.com>
2017-03-26 16:45:47 +0200

    Add meteorites

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # WIP D3.js Map Data Across the Globe
+
+### Todo
+
+- [ ] Add rolling zoom and pan
+- [ ] Position map to user's location on page load
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import * as d3 from 'd3';
-import * as topojson from 'topojson';
+import { select, zoom, event, geoMercator, geoPath, json, scaleSqrt, max } from 'd3';
+import { feature as topojsonFeature } from 'topojson';
 import 'styles';
 
 const url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
+const mapUrl = 'https://d3js.org/world-50m.v1.json';
 
 function visualize(data) {
-  const width = window.innerWidth * 0.95;
+  const width = window.innerWidth;
   const height = window.innerHeight * 0.8;
+  const white = 'rgba(255, 255, 255, 0.25)';
+  const purple = '#ce93d8';
 
-  const svg = d3.select('#map')
+  const svg = select('#map')
     .append('svg')
       .attr('class', 'map')
       .attr('width', width)
@@ -16,33 +19,61 @@ function visualize(data) {
 
   const map = svg.append('g');
 
-  // https://en.wikipedia.org/wiki/Web_Mercator
-  const projection = d3.geoMercator()
-    .translate([width / 2, height / 2])
-    .scale(width / 2 / Math.PI); // 300
+  svg.call(zoom()
+    .on('zoom', () => {
+      map.attr('transform', event.transform);
+    }));
 
-  const path = d3.geoPath()
+  // https://en.wikipedia.org/wiki/Web_Mercator
+  const projection = geoMercator()
+    .translate([width / 2, height])
+    .scale(width / 3);
+
+  const path = geoPath()
     .projection(projection);
 
-  d3.json('https://d3js.org/world-50m.v1.json', (err, world) => {
+  json(mapUrl, (err, world) => {
     if (err) throw err;
     map.selectAll('path')
-      .data(topojson.feature(world, world.objects.land).features)
+      .data(topojsonFeature(world, world.objects.countries).features)
       .enter()
       .append('path')
-        .attr('fill', 'rgba(255, 255, 255, 0.25)')
-        .attr('stroke', 'rgba(255, 255, 255, 0.25)')
+        .attr('fill', white)
+        .attr('stroke', white)
         .attr('strokeWidth', '0.5px')
         .style('vector-effect', 'non-scaling-stroke')
-        .attr('d', path)
-        .call(d3.zoom()
-          .on('zoom', () => {
-            map.attr('transform', d3.event.transform);
-          }));
+        .attr('d', path);
+
+    const meteoriteSize = scaleSqrt()
+      .range([1, 75])
+      .domain([0, max(data.features.map((d) => {
+        const { mass } = d.properties;
+        return mass ? mass * 1 : 0;
+      }))]);
+
+    const meteorites = map.append('g')
+      .selectAll('circle')
+        .data(data.features)
+        .enter()
+        .append('circle')
+          .attr('class', 'meteor')
+          .attr('cx', d => projection([
+            d.properties.reclong,
+            d.properties.reclat,
+          ])[0])
+          .attr('cy', d => projection([
+            d.properties.reclong,
+            d.properties.reclat,
+          ])[1])
+          .attr('r', d => meteoriteSize(d.properties.mass))
+          .attr('fill', white)
+          .attr('stroke', purple)
+          .attr('strokeWidth', '0.5px')
+          .style('vector-effect', 'non-scaling-stroke');
   });
 }
 
-d3.json(url, (err, data) => {
+json(url, (err, data) => {
   if (err) throw err;
   visualize(data);
 });
